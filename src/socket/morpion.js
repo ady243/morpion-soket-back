937b5c53821fff69ee11aab7d244deb52363cdc3
ady243 <masiviady7@gmail.com>
2024-06-02 19:59:24 +0200

    in progress

--- src/socket/morpion.js
+++ src/socket/morpion.js
@@ -1,23 +1,46 @@
-
-
+const animalNames = ["Lion", "Tigre", "Ours", "Girafe", "Zèbre", "Hippopotame", "Kangourou", "Panda", "Gorille", "Crocodile"];
+let players = {}; 
 let onlineUsers = [];
+
+const generateRandomAnimalName = () => {
+
+    const randomIndex = Math.floor(Math.random() * animalNames.length);
+    const animalName = animalNames.splice(randomIndex, 1)[0];
+    return animalName;
+};
 
 export const handleWebSocketConnections = (io) => {
     io.on('connection', (socket) => {
         console.log('New connection:', socket.id);
 
+        if (!players.X) {
+            players.X = { id: socket.id, name: generateRandomAnimalName() };
+            socket.emit('role', 'X');
+        } else if (!players.O) {
+            players.O = { id: socket.id, name: generateRandomAnimalName() };
+            socket.emit('role', 'O');
+        } else {
+            socket.emit('full');
+            socket.disconnect();
+            return;
+        }
+
+
+        io.emit('players', players);
+
         socket.on('move', (moveData) => {
             socket.broadcast.emit('move', moveData);
         });
 
-        // Gérer la déconnexion d'un joueur
         socket.on('disconnect', () => {
             console.log('Disconnected:', socket.id);
+            if (players.X?.id === socket.id) {
+                delete players.X;
+            } else if (players.O?.id === socket.id) {
+                delete players.O;
+            }
+            io.emit('players', players);
         });
-
-
-        // Gérer les messages du chat
-        //ecoute la connection d'un user 
         socket.on("addNewUser", (userId)=>{
             if (!onlineUsers.some(user => user.userId === userId)) {
                 onlineUsers.push({
@@ -41,6 +64,6 @@ export const handleWebSocketConnections = (io) => {
             onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id);
             io.emit("getOnlineUsers", onlineUsers);
         })
- 
+        
     });
 };
