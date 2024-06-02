b21e2207bf949bf5083fb5a1252c9fe4f284e889
ady243 <masiviady7@gmail.com>
2024-06-02 18:50:59 +0200

    update backend

--- src/socket/morpion.js
+++ src/socket/morpion.js
@@ -1,3 +1,7 @@
+
+
+let onlineUsers = [];
+
 export const handleWebSocketConnections = (io) => {
     io.on('connection', (socket) => {
         console.log('New connection:', socket.id);
@@ -10,5 +14,33 @@ export const handleWebSocketConnections = (io) => {
         socket.on('disconnect', () => {
             console.log('Disconnected:', socket.id);
         });
+
+
+        // Gérer les messages du chat
+        //ecoute la connection d'un user 
+        socket.on("addNewUser", (userId)=>{
+            if (!onlineUsers.some(user => user.userId === userId)) {
+                onlineUsers.push({
+                    userId,
+                    socketId: socket.id
+                });
+            }
+            io.emit("getOnlineUsers", onlineUsers);
+        })
+
+        //add message
+        socket.on("sendMessage", (message)=>{
+            const user = onlineUsers.find(user => user.userId === message.recipientId);
+
+            if(user){
+                io.to(user.socketId).emit("getMessage", message);
+            }
+        })
+
+        socket.on("disconnect",()=>{
+            onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id);
+            io.emit("getOnlineUsers", onlineUsers);
+        })
+ 
     });
 };
--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,56 +1,6 @@
 
-import { use } from "chai";
-import { Server } from "socket.io";
-
-const io = new Server( process.env.SOCKET_PORT, {
-    cors: {
-        origin: "http://localhost:5173",
-    }
-});
-
-let onlineUser = [];
-
-
-io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
-
-    socket.on("addNewUser", (userId) => {
-        ! onlineUser.some((user) => user.userId === userId) &&
-        onlineUser.push({
-            userId,
-            socketId: socket.id,
-        });
-
-        console.log("online user",onlineUser);
-
-        io.emit("getOnlineUser", onlineUser);
-
+export const handleWebSocketConnectionsChat = (io) => {
+    io.on("connection", (socket) => {
+     console.log("New client connected", socket.id);
     });
-
-    //add Message
-    socket.on("addMessage", (message) => {
-        const user = onlineUser.find((user) => user.userId === message.respientId);
-        user && io.to(user.socketId).emit("getMessage", message);
-        if(user) {
-            io.to(user.socketId).emit("getMessage", message);
-            io.to(socket.id).emit("getNotification", {
-                senderId: message.senderId,
-                isRead: false,
-                date: new Date(),
-            });
-        }
-    });
-
-    socket.on("disconnect", () => {
-        console.log("a user disconnected", socket.id);
-        onlineUser = onlineUser.filter((user) => user.socketId !== socket.id);
-        io.emit("getOnlineUser", onlineUser);
-    });
-
-});
-
-
-
-
-
-
+};
